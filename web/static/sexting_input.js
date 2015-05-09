a8f142f3520daf400e6c6b1881bd416eef81289f
Tom Wadley <tom@tomwadley.net>
2015-05-09 11:04:06 +0100

    Update Rosa's key layout and have no unmodified keys

--- web/static/sexting_input.js
+++ web/static/sexting_input.js
@@ -1,37 +1,38 @@
+// Makey Makey key mappings
 
-charKeys = [72, 73, 74, 75, 76, 77, 78, 79, 80, 82];
-modKeys = [65, 66, 67, 68, 69, 70];
+// UP     = 65 = a
+// DOWN   = 66 = b
+// LEFT   = 67 = c
+// RIGHT  = 68 = d
+// SPACE  = 69 = e
+// CLICK  = 70 = f
+
+// D5     = 71 = g
+// D4     = 72 = h
+// D3     = 73 = i
+// D2     = 74 = j
+// D1     = 75 = k
+// D0     = 76 = l
+
+// A5     = 77 = m
+// A4     = 78 = n
+// A3     = 79 = o
+// A2     = 80 = p
+// A1     = 81 = q
+// A0     = 82 = r
+
+modKeys = [65, 66, 67, 68, 69, 70, 81];
 shiftKey = 71;
+otherKeys = [72, 73, 74, 75, 76, 77, 78, 79, 80, 82];
 
-allKeys = charKeys.concat(modKeys).concat([shiftKey]);
-
-// D5 = 71
-// D4 = 72
-// D3 = 73
-// D2 = 74
-// D1 = 75
-// D0 = 76
-
-// A5 = 77
-// A4 = 78
-// A3 = 79
-// A2 = 80
-// A1 = 81
-// A0 = 82
-
-// UP     = 65
-// DOWN   = 66
-// LEFT   = 67
-// RIGHT  = 68
-// SPACE  = 69
-// CLICK  = 70
+allKeys = otherKeys.concat(modKeys).concat([shiftKey]);
 
 mappings = {
-  0: { 72: ' ', 73: "ENTER", 74: "BACKSPACE" },
+  81: { 72: ' ', 71: "ENTER", 74: "BACKSPACE" },
   65: { 72: ';', 73: '-', 74: ':', 75: '!', 76: ',', 78: '.', 82: '?' },
   66: { 72: '2', 73: '0', 74: '1', 75: '3', 76: '5', 77: '7', 78: '6', 79: '9', 80: '8', 82: '4' },
-  67: { 72: 'r', 73: 't', 74: 's', 75: 'q', 76: 'o', 78: 'n', 82: 'p' },
-  68: { 72: 'k', 73: 'm', 74: 'l', 75: 'j', 76: 'h', 82: 'i' },
+  67: { 72: 'k', 73: 'm', 74: 'l', 75: 'j', 76: 'h', 82: 'i' },
+  68: { 72: 'r', 73: 't', 74: 's', 75: 'q', 76: 'o', 78: 'n', 82: 'p' },
   69: { 72: 'x', 73: 'z', 74: 'y', 75: 'w', 76: 'u', 82: 'v' },
   70: { 72: 'e', 73: 'g', 74: 'f', 75: 'd', 76: 'b', 78: 'a', 82: 'c' }
 };
@@ -73,22 +74,24 @@ function processBackspace() {
   }
 }
 
-function currentModKey() {
+function currentKeySet() {
   modKey = 0
   $.each(modKeys, function(i, key) {
     if (keyDown[key]) {
       modKey = key;
     }
   });
-  return modKey;
+  if (modKey in mappings) {
+    return mappings[modKey];
+  }
+  return [];
 }
 
 function keyPressed(key) {
-  modKey = currentModKey();
+  keySet = currentKeySet();
 
-  d = mappings[modKey];
-  if (key in d) {
-    character = d[key];
+  if (key in keySet) {
+    character = keySet[key];
     if (character.length == 1) {
       if (keyDown[shiftKey]) {
         character = character.toUpperCase();
